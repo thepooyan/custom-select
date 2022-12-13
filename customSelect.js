8ddefb1161a1a1118d816c6d0624191829ee1dfd
Pooyan <thepooyan@gmail.com>
2022-12-13 16:10:31 +0330

    active being determined by select default value

--- customSelect.js
+++ customSelect.js
@@ -2,10 +2,11 @@ export function initCustomSelect(className = 'select') {
   document.querySelectorAll(`.${className}`).forEach(wrapper => {
 
     let options = wrapper.querySelectorAll('option');
-    let active = options[0];
     let tag = document.createElement('span');
     let list = document.createElement('div');
     let select = wrapper.querySelector('select');
+    let active = Object.values(options).find(opt=>opt.value===select.value);
+    console.log(active);
 
     list.classList.add('list');
     wrapper.value = active.value;
@@ -22,6 +23,7 @@ export function initCustomSelect(className = 'select') {
       span.onclick = _ => {
         tag.innerHTML = option.innerHTML;
         wrapper.value = option.value;
+        wrapper.onchange();
         select.value = option.value;
       }
 
